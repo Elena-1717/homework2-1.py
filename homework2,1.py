068e87432c96be46c34f1c04dfa9495e2b2c7c58
Elena-1717 <kea5599@mail.ru>
2024-07-03 11:10:52 +0300

    Add files via upload

---
+++ homework2,1.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Hello world")
+example = 'Топинамбур'
+print("Hello " + example)
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1:10:2])
+dz = 12
+zch = 1.5
+name = 'Python,'
+zd1 = 1.5/12
+print('Курс:',name,'всего задач:',dz,'затрачено часов:',zch,'среднее время выполнения:',zd1,'часа')
+
+
+
+
